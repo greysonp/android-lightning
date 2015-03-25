c062c10b045c087092931cab2ce4ab8f11ba201c
Greyson Parrelli <greyson.parrelli@gmail.com>
2015-03-24 23:56:10 -0700

    Removed unused imports.

--- app/src/main/java/com/greysonparrelli/lightning/EditorActivity.java
+++ app/src/main/java/com/greysonparrelli/lightning/EditorActivity.java
@@ -2,8 +2,6 @@ package com.greysonparrelli.lightning;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 
